8d1e4dea6692232919967fa7ee340dc7e0bb9176
Anthony Davis <anthony.m.davis.26@gmail.com>
2024-01-09 12:50:57 -0500

    Rotator style, size, opacity changes + some general css

--- scripts/rotator.js
+++ scripts/rotator.js
@@ -17,41 +17,46 @@ class Rotator {
         const rightArrow = this.rotatorElement.querySelector(this.options.rightArrowSelector);
 
         leftArrow.addEventListener('click', () => this.rotateLeft());
-        rightArrow.addEventListener('click', () => this.rotateRight());
-        
-        const middleIndex = Math.floor(this.items.length / 2) - 1;
-        this.items.forEach((item, index) => {
-            if (index === middleIndex) {
-                item.classList.add('large');
-            } else {
-                item.classList.remove('large');
-            }
-        });
+        rightArrow.addEventListener('click', () => this.rotateRight()); 
+        this.updateItemClasses();
+    }
+
+    rotateRight() {
+        this.rotatorContents.appendChild(this.items[0]);
+        this.items = Array.from(this.rotatorContents.children); 
+        this.updateItemClasses();
     }
 
     rotateLeft() {
-        this.rotatorContents.appendChild(this.items[0]);
-        this.items = Array.from(this.rotatorContents.children);
-        this.updateItemClasses();
-    }
-    
-    rotateRight() {
         const lastItem = this.items[this.items.length - 1];
         this.rotatorContents.insertBefore(lastItem, this.items[0]);
-        this.items = Array.from(this.rotatorContents.children);
+        this.items = Array.from(this.rotatorContents.children); 
         this.updateItemClasses();
     }
-    
+
     updateItemClasses() {
-        const middleIndex = Math.floor(this.items.length / 2) - 1;
-        this.items.forEach((item, index) => {
-            if (index === middleIndex) {
-                item.classList.add('large');
-            } else {
-                item.classList.remove('large');
-            }
-        });
-    }     
+        const n = this.items.length;
+        const startHeight = 25;
+        const heightMultiplier = 2;
+        const startOpacity = 20;
+        const opacityMultiplier = 3;
+        var opacity = startOpacity;
+        var height = startHeight;
+        const midIndex = Math.floor(this.items.length / 2);
+        for (let i = 0; i < midIndex; i++) {
+            this.items[i].style.setProperty('width', height + '%');
+            height *= heightMultiplier;
+            this.items[i].style.setProperty('opacity', opacity + '%');
+            opacity *= opacityMultiplier;
+        }
+        for (let i = midIndex; i < n; i++) {
+            this.items[i].style.setProperty('width', height + '%');
+            height /= heightMultiplier;
+            this.items[i].style.setProperty('opacity', opacity + '%');
+            opacity /= opacityMultiplier;
+        }
+        this.items[midIndex].style.setProperty('opacity', '100%');
+    }
 }
 
 document.querySelectorAll('.rotator').forEach((rotatorElement) => {
